0e00d91b5e190b26b84be38fd6979c7418322302
ShatteredChungus <92784608+ShatteredChungus@users.noreply.github.com>
2022-03-22 05:15:25 -0400

    added button functionality

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,82 @@
 // declaration block
-const startTime = 80;
-let time = startTime;
-const timerEl = document.getElementById('timer');
-const questionEl = document.getElementById('question');
-const answerOneEl = document.getElementById('answerOne');
-const answerTwoEl = document.getElementById('answerTwo');
-const answerThreeEl = document.getElementById('answerThree');
-const answerFourEl = document.getElementById('answerFour');
 
-// timer
+const startTime = 80;
+let questionNumber = 0;
+let time = startTime;
+
+const questions = ["Commonly used data types do NOT include:","The condition in an 'If/else' statement is enclosed with _____","Arrays in javascript can be used to store ______","String Values must be stored in _____ when being assigned to variables.","A very useful tool during development and debugging for printing content to the debugger is:","All Done!","High Scores"];
+
+const answersOne = ["1. Strings","1. Quotes","1. Numbers and strings","1. Commas","1. Javascript"];
+
+const answersTwo = ["2. Booleans","2. Curly brackets","2. Other Arrays","2. Curly brackets","2. Terminal/bash"];
+
+const answersThree = ["3. Alerts","3. Parenthesis","3. Booleans","3. Quotes","3. For loops"];
+
+const answersFour = ["4. Numbers","4. Square brackets","4. All of the above","4. Parenthesis","4. console.log"];
+
+const timerEl = document.getElementById('timer');
+
+const questionEl = document.getElementById('question');
+
+const startButton = document.getElementById('startBtn');
+
+const answerOneButton = document.getElementById('answerOneButton');
+
+const answerTwoButton = document.getElementById('answerTwoButton');
+
+const answerThreeButton = document.getElementById('answerThreeButton');
+
+const answerFourButton = document.getElementById('answerFourButton');
+
+
+
+startButton.addEventListener('click',()=>{
+    quiz();
+    timeStart();
+});
+
+answerOneButton.addEventListener('click',quiz);
+answerTwoButton.addEventListener('click', quiz);
+answerThreeButton.addEventListener('click', quiz);
+answerFourButton.addEventListener('click', quiz);
+
+
+
+
+
+function quiz () {
+
+//hide start button - unhide other buttons
+startButton.classList.add('hide');
+answerOneButton.classList.remove('hide');
+answerTwoButton.classList.remove('hide');
+answerThreeButton.classList.remove('hide');
+answerFourButton.classList.remove('hide');
+
+
+// page setup
+questionEl.textContent = (questions[questionNumber]);
+answerOneButton.textContent = (answersOne[questionNumber]);
+answerTwoButton.textContent = (answersTwo[questionNumber]);
+answerThreeButton.textContent =(answersThree[questionNumber]);
+answerFourButton.textContent = (answersFour[questionNumber]);
+
+questionNumber++;
+
+}
+
+function timeStart() {
+
 setInterval(countdown, 1000);
 
 function countdown() {
+    // start timer/score
+
     if (time > -1) {
     timerEl.innerHTML = ("Score: " + time);
     time --;
     }
 }
 
-//questions
+}
 
-questionEl.innerHTML = ("Coding Quiz Challenge: Incorrect answers detract from score/time. Select 'Start Quiz' when ready.");
-
-questionEl.innerHTML = ("Commonly used data types do NOT include:");
-
-questionEl.innerHTML = ("The condition in an 'If/else' statement is enclosed with _____");
-
-questionEl.innerHTML = ("Arrays in javascript can be used to store ______");
-
-questionEl.innerHTML = ("String Values must be stored in _____ when being assigned to variables.");
-
-questionEl.innerHTML = ("A very useful tool during development and debugging for printing content to the debugger is:");
-
-questionEl.innerHTML = ("All Done!")
-
-questionEl.innerHTML = ("High Scores");
-
-//answers
-
-answerOneEl.innerHTML = ("1. Strings")
-answerOneEl.innerHTML = ("1. Quotes")
-answerOneEl.innerHTML = ("1. Numbers and strings")
-answerOneEl.innerHTML = ("1. Commas")
-answerOneEl.innerHTML = ("1. Javascript")
-
-answerTwoEl.innerHTML = ("2. Booleans")
-answerTwoEl.innerHTML = ("2. Curly brackets")
-answerTwoEl.innerHTML = ("2. Other Arrays")
-answerTwoEl.innerHTML = ("2. Curly brackets")
-answerTwoEl.innerHTML = ("2. Terminal/bash")
-
-answerThreeEl.innerHTML =("3. Alerts")
-answerThreeEl.innerHTML =("3. Parenthesis")
-answerThreeEl.innerHTML =("3. Booleans")
-answerThreeEl.innerHTML =("3. Quotes")
-answerThreeEl.innerHTML =("3. For loops")
-
-answerFourEl.innerHTML = ("4. Numbers")
-answerFourEl.innerHTML = ("4. Square brackets")
-answerFourEl.innerHTML = ("4. All of the above")
-answerFourEl.innerHTML = ("4. Parenthesis")
-answerFourEl.innerHTML = ("4. console.log")
